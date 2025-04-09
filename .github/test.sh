c4437aa871db5bfb4b5cda4e665295abc82d5347
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-04-09 11:32:56 -0400

    ci: updates workflow test and adds coverage percentage

--- .github/test.sh
+++ .github/test.sh
@@ -1,13 +1,19 @@
 #!/bin/dash
-pip install -e git+https://github.com/eol-uchile/uchileedxlogin@8cb702fe18c26b29c0667c660c24ee75a03c9ec9#egg=uchileedxlogin
-pip install -e /openedx/requirements/eol_xblock_completion
+pip install -e git+https://github.com/eol-uchile/uchileedxlogin@0.0.1#egg=uchileedxlogin
+pip install -e /openedx/requirements/app
+pip install pytest-cov genbadge[coverage]
 
-cd /openedx/requirements/eol_xblock_completion
+cd /openedx/requirements/app
 cp /openedx/edx-platform/setup.cfg .
+sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=xblockcompletion/ --cov-report term-missing --cov-report xml:reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
+
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/eol_xblock_completion
+cd /openedx/requirements/app
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest xblockcompletion/tests.py
+
+echo "[run]\nomit = xblockcompletion/migrations/*" > .coverage
+genbadge coverage
